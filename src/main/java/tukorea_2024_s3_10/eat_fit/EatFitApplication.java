bcc9d513c1abc669e1601d4e310e4dfde50799d3
kwonjh0406 <kwonjh0406@gmail.com>
2025-02-05 15:15:03 +0900

    init: Initialize Spring Boot with Spring Data JPA, Spring Web, MySQL Driver, Lombok

---
+++ src/main/java/tukorea_2024_s3_10/eat_fit/EatFitApplication.java
@@ -0,0 +1,13 @@
+package tukorea_2024_s3_10.eat_fit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatFitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tukorea_2024_s3_10/eat_fit/EatFitApplicationTests.java
@@ -0,0 +1,13 @@
+package tukorea_2024_s3_10.eat_fit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
