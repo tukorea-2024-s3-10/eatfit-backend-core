68d704a03f1bd1b8fb109ea01c985a2b4dc3ddc7
GeumMW <rmaalsdn08@naver.com>
2025-02-15 17:18:53 +0900

    유저 목표 섭취량 조회 기능 구현

---
+++ src/main/java/tukorea_2024_s3_10/eat_fit/application/dto/IntakeGoalResponse.java
@@ -0,0 +1,29 @@
+package tukorea_2024_s3_10.eat_fit.application.dto;
+
+import lombok.Getter;
+import tukorea_2024_s3_10.eat_fit.domain.user.entity.UserIntakeGoal;
+
+@Getter
+public class IntakeGoalResponse {
+    private int calorieGoal;       // 칼로리
+    private int sodiumGoal;        // 나트륨
+    private double carbohydrateGoal;  // 탄수화물
+    private double sugarGoal;         // 당류
+    private double fatGoal;           // 지방
+    private double transFatGoal;      // 트랜스지방
+    private double saturatedFatGoal;  // 포화지방
+    private int cholesterolGoal;   // 콜레스테롤
+    private double proteinGoal;       // 단백질
+
+    public IntakeGoalResponse(UserIntakeGoal userIntakeGoal) {
+        calorieGoal = userIntakeGoal.getCalorieGoal();
+        sodiumGoal = userIntakeGoal.getSodiumGoal();
+        carbohydrateGoal = userIntakeGoal.getCarbohydrateGoal();
+        sugarGoal = userIntakeGoal.getSugarGoal();
+        fatGoal = userIntakeGoal.getFatGoal();
+        transFatGoal = userIntakeGoal.getTransFatGoal();
+        saturatedFatGoal = userIntakeGoal.getSaturatedFatGoal();
+        cholesterolGoal = userIntakeGoal.getCholesterolGoal();
+        proteinGoal = userIntakeGoal.getProteinGoal();
+    }
+}
--- src/main/java/tukorea_2024_s3_10/eat_fit/application/service/UserService.java
+++ src/main/java/tukorea_2024_s3_10/eat_fit/application/service/UserService.java
@@ -3,7 +3,10 @@ package tukorea_2024_s3_10.eat_fit.application.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import tukorea_2024_s3_10.eat_fit.application.dto.IntakeGoalResponse;
+import tukorea_2024_s3_10.eat_fit.application.util.UserGoalCalculator;
 import tukorea_2024_s3_10.eat_fit.domain.auth.Role;
+import tukorea_2024_s3_10.eat_fit.domain.user.entity.UserIntakeGoal;
 import tukorea_2024_s3_10.eat_fit.infrastructure.security.SecurityUtil;
 import tukorea_2024_s3_10.eat_fit.application.dto.ProfileResponse;
 import tukorea_2024_s3_10.eat_fit.presentation.user.dto.ProfileEditRequest;
@@ -13,6 +16,8 @@ import tukorea_2024_s3_10.eat_fit.domain.user.entity.UserProfile;
 import tukorea_2024_s3_10.eat_fit.domain.user.repository.UserGoalRepository;
 import tukorea_2024_s3_10.eat_fit.domain.user.repository.UserProfileRepository;
 import tukorea_2024_s3_10.eat_fit.domain.user.repository.UserRepository;
+
+import static tukorea_2024_s3_10.eat_fit.application.util.UserGoalCalculator.recommendUserGoal;
 
 @Service
 @RequiredArgsConstructor
@@ -36,6 +41,12 @@ public class UserService {
                 .height(profileInitRequest.getHeight())
                 .user(user)
                 .build();
+
+
+        // 유저 맞춤 섭취량 설정
+        UserIntakeGoal userIntakeGoal = recommendUserGoal(userProfile);
+        userGoalRepository.save(userIntakeGoal);
+
         userProfileRepository.save(userProfile);
         user.changeRole(Role.ROLE_USER);
         userRepository.save(user);
@@ -64,8 +75,20 @@ public class UserService {
         userProfile.setTargetWeight(profileEditRequest.getTargetWeight());
         userProfile.setDisease(profileEditRequest.getDisease());
 
+        // 유저 맞춤 섭취량 설정
+        UserIntakeGoal userIntakeGoal = recommendUserGoal(userProfile);
+        userGoalRepository.save(userIntakeGoal);
+
         userProfileRepository.save(userProfile);
 
 
+    }
+
+    public IntakeGoalResponse getIntakeGoal() {
+        Long currentUserId = SecurityUtil.getCurrentUserId();
+
+        UserIntakeGoal userIntakeGoal = userGoalRepository.findById(currentUserId).get();
+
+        return new IntakeGoalResponse(userIntakeGoal);
     }
 }
