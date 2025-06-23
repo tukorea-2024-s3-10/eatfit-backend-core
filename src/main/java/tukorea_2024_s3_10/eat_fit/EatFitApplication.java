8d7e1da8356e45d85c95158e6fcd3574e41f6aeb
GeumMW <rmaalsdn08@naver.com>
2025-06-24 00:43:25 +0900

    TimeZone 변경

--- src/main/java/tukorea_2024_s3_10/eat_fit/EatFitApplication.java
+++ src/main/java/tukorea_2024_s3_10/eat_fit/EatFitApplication.java
@@ -1,14 +1,22 @@
 package tukorea_2024_s3_10.eat_fit;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 @EnableBatchProcessing
 public class EatFitApplication {
+
+	@PostConstruct
+	public void setTimeZone() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EatFitApplication.class, args);
