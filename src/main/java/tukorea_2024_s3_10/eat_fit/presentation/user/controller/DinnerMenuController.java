8d4f998842a558b310bbeb784922bdeb6038c7f2
GeumMW <rmaalsdn08@naver.com>
2025-06-23 20:56:57 +0900

    저메추 스웨거 작성

--- src/main/java/tukorea_2024_s3_10/eat_fit/presentation/user/controller/DinnerMenuController.java
+++ src/main/java/tukorea_2024_s3_10/eat_fit/presentation/user/controller/DinnerMenuController.java
@@ -1,5 +1,6 @@
 package tukorea_2024_s3_10.eat_fit.presentation.user.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,7 @@ public class DinnerMenuController {
     private final DinnerMenuService dinnerMenuService;
 
     @GetMapping("/dinner")
+    @Operation(summary = "저녁 메뉴 추천", description = "로그인된 사용자의 섭취량을 분석해 저녁 메뉴를 추천합니다")
     public ResponseEntity<ApiResponse<DinnerMenuResponse>> getDinnerMenu() {
         DinnerMenuResponse response = dinnerMenuService.getDinnerMenu();
         return ResponseEntity.ok(ApiResponse.success(response));
