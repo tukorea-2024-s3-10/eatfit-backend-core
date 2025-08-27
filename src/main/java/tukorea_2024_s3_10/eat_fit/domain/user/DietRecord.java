06328cba450fa9b5e2120bc46be02a94f54f5b2e
GeumMW <rmaalsdn08@naver.com>
2025-08-27 14:23:18 +0900

    db꼬임 테스트

--- src/main/java/tukorea_2024_s3_10/eat_fit/domain/user/DietRecord.java
+++ src/main/java/tukorea_2024_s3_10/eat_fit/domain/user/DietRecord.java
@@ -15,7 +15,7 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class DietRecord {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private Long userId;
